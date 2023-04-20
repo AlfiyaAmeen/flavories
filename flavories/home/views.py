012b6528c0f0aba38ccc4d317b3405ec6fa204b3
Alfiya <alfiyanasar3@gmail.com>
2023-04-20 12:07:19 +0530

    created urls.py in homeapp username,password and mail id to checkin verification done.then registration process completed,newly registered user in admin page.username password is log in page verification is done

--- flavories/home/views.py
+++ flavories/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from django.contrib import auth
+from django.contrib.auth.models import User
+
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -12,8 +14,8 @@ def login(request):
 def register(request):
     return render(request,"register.html")
 def loginsub(request):
-    uname=request.GET["user"]
-    pname=request.GET["pas"]
+    uname=request.POST["user"]
+    pname=request.POST["pas"]
     u=auth.authenticate(username=uname,password=pname)
     if u:
         auth.login(request,u)
@@ -21,11 +23,29 @@ def loginsub(request):
 
     return render (request,"login.html",{"val":u})
 def regsub(request):
-    fname=request.GET["fname"]
-    lname=request.GET["lname"]
-    uname=request.GET["uname"]
-    email=request.GET["email"]
-    pas=request.GET["pas"]
-    repas=request.GET["repas"]
-    return render(request,"text.html",{"val1":fname,"val2":uname})
+    fname=request.POST["fname"]
+    lname=request.POST["lname"]
+    uname=request.POST["uname"]
+    email=request.POST["email"]
+    pas=request.POST["pas"]
+    repas=request.POST["repas"]
+    if pas==repas:
+        if User.objects.filter(username=uname):
+            msg="username is already taken"
+            return render(request,"text.html",{"val":msg})
+ 
+        elif User.objects.filter(email=email):
+            msg="Email is already taken" 
+            return render(request,"text.html",{"val":msg})
+ 
+
+        else:
+            user=User.objects.create_user(first_name=fname,last_name=lname,username=uname,email=email,
+            password=pas)
+            user.save();
+            return redirect("/")
+    else:
+        msg="Password invalid"
+        return render(request,"text.html",{"val":msg})
+ 
     
