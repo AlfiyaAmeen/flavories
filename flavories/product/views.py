f12a8289307b2f01fa6410979bd9e94608a8d133
Alfiya <alfiyanasar3@gmail.com>
2023-05-05 11:14:15 +0530

    error occured so committed

--- flavories/product/views.py
+++ flavories/product/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import fruits
 # Create your views here.
 def product(request):
-    return render(request,"text.html")
+    iname=request.GET['id']
+    obj=fruits.objects.get(id=iname)
+    return render(request,"about.html",{"data":obj})
